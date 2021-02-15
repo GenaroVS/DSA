5a5c98fda95497df6c27fd64b566847e0292c38f
Genaro V Salinas <gvsalinas3@gmail.com>
2021-02-15 14:32:17 -0800

    Added binary number insertion to prefix trees

--- trie.js
+++ trie.js
@@ -16,6 +16,18 @@ class Trie {
     node.isWord = true;
   }
 
+  insertBin(num, len = 32) {
+    var node = this;
+    for (var i = len - 1; i >= 0; i--) {
+      var bit = (num >> i) & 1;
+      if (!node.children[bit]) {
+        node.children[bit] = new Trie();
+      }
+      node = node.children[bit];
+    }
+    node.baseTenNum = num;
+  }
+
   search(word) {
     var node = this;
     for (var i = 0; i < word.length; i++) {
@@ -23,7 +35,7 @@ class Trie {
       if (!node.children[char]) return false;
       node = node.children[char];
     }
-    return node.isWord ? true : false;
+    return node.isWord;
   }
 
   startsWith(prefix) {
@@ -35,4 +47,27 @@ class Trie {
     }
     return true;
   }
+
+  countWords(charMap, firstChar, valid = false, count = 0) {
+    var node = this;
+    if (node.wordCount > 0 && valid) {;
+      count += node.wordCount;
+    }
+    for (var char in node.children) {
+      if (charMap[char]) {
+        if (char == firstChar) {
+          count = node.children[char]
+            .countWords(charMap, firstChar, true, count);
+        } else {
+          count = node.children[char]
+            .countWords(charMap, firstChar, valid, count);
+        }
+      }
+    }
+
+    return count;
+  }
 }
+
+
+
