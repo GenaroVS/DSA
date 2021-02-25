a352e6f4a4e4e730b07b791d2dd639c4b78f3ffa
Genaro V Salinas <gvsalinas3@gmail.com>
2021-02-24 22:24:07 -0800

    Added LRUCache data structure

---
+++ LRUcache.js
@@ -0,0 +1,48 @@
+const DDL = require('./doubly-linked-list.js');
+
+/**
+ * @param {number} capacity
+ */
+class LRUCache {
+  constructor(capacity) {
+    this.list = new DDL(null, null),
+    this.map = {},
+    this.size = 0,
+    this.cap = capacity
+  }
+
+  /**
+   * @param {number} key
+   * @return {number}
+   */
+  get(key){
+    var node = this.map[key];
+    if (node) {
+      var value = node.val;
+      this.list.remove(node)
+      this.map[key] = this.list.push(key, value);
+      return value;
+    }
+    return -1;
+  }
+
+  /**
+  * @param {number} key
+  * @param {number} value
+  * @return {void}
+  */
+  set(key, value) {
+    var node = this.map[key];
+    if (node) {
+      this.list.remove(node);
+      this.size--;
+    }
+    this.map[key] = this.list.push(key, value);
+    this.size++;
+    if (this.size > this.cap) {
+      delete this.map[this.list.head.key];
+      this.list.remove(this.list.head);
+      this.size--;
+    }
+  }
+}
