54296b2e971b5def09578a47ae631a80155f001c
Genaro V Salinas <gvsalinas3@gmail.com>
2021-03-09 18:42:28 -0800

    Added basic BST

---
+++ binarySearchTree.js
@@ -0,0 +1,70 @@
+class BST {
+  constructor(val = null, left = null, right = null) {
+    this.val = val;
+    this.left = left;
+    this.right = right
+  }
+
+  search(root = this.root, val) {
+    var node = root;
+
+    while (node) {
+      if (val < node.val) {
+        node = node.left;
+      } else if (val > node.val) {
+        node = node.right;
+      } else if (val === node.val) {
+        return node;
+      } else {
+        break;
+      }
+    }
+
+    return null;
+  }
+
+  insert(root = this.root, val) {
+    if (!root) return new TreeNode(val);
+    var node = root;
+
+    while (true) {
+      if (val > node.val) {
+        if (!node.right) {
+          node.right = new TreeNode(val);
+          break;
+        }
+        node = node.right;
+      } else if (val < node.val) {
+        if (!node.left) {
+          node.left = new TreeNode(val);
+          break;
+        }
+        node = node.left;
+      }
+    }
+
+    return root;
+  }
+
+  delete(root = this.root, key) {
+    if (!root) return null;
+
+    if (key < root.val) {
+      root.left = deleteNode(root.left, key);
+    } else if (key > root.val) {
+      root.right = deleteNode(root.right, key);
+    } else {
+      if (!root.left) {
+        return root.right;
+      } else if (!root.right) {
+        return root.left;
+      }
+
+      var temp = minVal(root.right);
+      root.val = temp.val
+      root.right = deleteNode(root.right, temp.val);
+    }
+
+    return root;
+  }
+}
